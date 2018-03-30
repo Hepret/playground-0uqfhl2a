c98b5a4367cb8ca8521889c0790ef909c9588aa9
Administrator <admin@local.host>
2018-03-31 00:50:00 +0530

    More Pythonic

--- project/operations_python_objects.py
+++ project/operations_python_objects.py
@@ -1,55 +1,41 @@
 import math
 
-from yml_pareser import ConfigObj
-
 
 def square(in_put):
-    if isinstance(in_put, (int,float,long)):
+    if isinstance(in_put,(int,float,long)):
         value = in_put * in_put
 
-    elif isinstance(in_put, list):
-        value = []
-        for i in in_put:
-            value.append(i * i)
-    elif isinstance(in_put, dict):
-        value = {}
-        for key in in_put.keys():
-            value[key] = in_put[key] * in_put[key]
+    elif isinstance(in_put,list):
+        value = [i * i for i in in_put]
+
+    elif isinstance(in_put,dict):
+        value = {k: in_put[k] * in_put[k] for k in in_put.keys()}
     else:
         raise Exception("Data Type to operate has not been implemented")
     return value
 
 
 def cube(in_put):
-    if isinstance(in_put, (int,float,long)):
+    if isinstance(in_put,(int,float,long)):
         value = in_put * in_put * in_put
-
-    elif isinstance(in_put, list):
-        value = []
-        for i in in_put:
-            value.append(i * i * i)
-    elif isinstance(in_put, dict):
-        value = {}
-        for key in in_put.keys():
-            value[key] = in_put[key] * in_put[key] * in_put[key]
+    elif isinstance(in_put,list):
+        value = [i * i * i for i in in_put]
+    elif isinstance(in_put,dict):
+        value = {k: in_put[k] * in_put[k] * in_put[k] for k in in_put.keys()}
     else:
         raise Exception("Data Type to operate has not been implemented")
     return value
 
 
 def factorial(in_put):
-    if isinstance(in_put, (int,float,long)):
+    if isinstance(in_put,(int,float,long)):
         value = math.factorial(in_put)
 
-    elif isinstance(in_put, list):
-        value = []
-        for i in in_put:
-            value.append(math.factorial(i))
+    elif isinstance(in_put,list):
+        value = [math.factorial(i) for i in in_put]
 
-    elif isinstance(in_put, dict):
-        value = {}
-        for key in in_put.keys():
-            value[key] = math.factorial(in_put[key])
+    elif isinstance(in_put,dict):
+        value = {key: math.factorial(in_put[key]) for key in in_put.keys()}
     else:
         raise Exception("Data Type to operate has not been implemented")
     return value
@@ -59,5 +45,5 @@ class Transform:
     def __init__(self):
         pass
 
-    def execute(self, method, params):
+    def execute(self,method,params):
         return eval(method)(params)
--- project/test_operations_python_objects.py
+++ project/test_operations_python_objects.py
@@ -1,12 +1,28 @@
-import pytest 
+import pytest
+from operations_python_objects import Transform
 
 @pytest.fixture(scope='function')
 def initi():
     obj = Transform()
     return obj
 
-@pytest.mark.parametrize("in_put,out_put",[('square;5',25),('factorial;[1,2,3]',[1,2,6])])
-def test_operations(initi,in_put,out_put):
+@pytest.mark.parametrize("in_put,out_put",[('factorial;5',120),('factorial;[1,2,3]',[1,2,6]),("factorial;{'a':1,'b':2,'c':3}",{'a':1,'b':2,'c':6})])
+def test_operations1(initi,in_put,out_put):
+    in_put1=in_put.split(';')[0]
+    in_put2 = in_put.split(';')[1]
+    op = initi.execute(in_put1,eval(in_put2))
+    assert op==out_put
+
+
+@pytest.mark.parametrize("in_put,out_put",[('cube;5',125),('cube;[1,2,3]',[1,8,27]),("cube;{'a':1,'b':2,'c':3}",{'a':1,'b':8,'c':27})])
+def test_operations2(initi,in_put,out_put):
+    in_put1=in_put.split(';')[0]
+    in_put2 = in_put.split(';')[1]
+    op = initi.execute(in_put1,eval(in_put2))
+    assert op==out_put
+
+@pytest.mark.parametrize("in_put,out_put",[('square;5',25),('square;[1,2,3]',[1,4,9]),("square;{'a':1,'b':2,'c':3}",{'a':1,'b':4,'c':9})])
+def test_operations3(initi,in_put,out_put):
     in_put1=in_put.split(';')[0]
     in_put2 = in_put.split(';')[1]
     op = initi.execute(in_put1,eval(in_put2))
